de68159771c9d40607e92b4458594d17de395efe
Jose Esteban <josegg1897@gmail.com>
2022-12-26 15:22:16 -0400

    The end of React.createElement

---
+++ src/App.js
@@ -0,0 +1,43 @@
+const Pet = ({name, animal, breed}) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1",{},name),
+    React.createElement("h2",{},animal),
+    React.createElement("h2",{},breed)
+  ])
+}
+
+const App = () => {
+  const animals = [
+    {
+      name:'Chivila',
+      animal: 'dog',
+      breed: 'viralata'
+    },
+    {
+      name:'Cow',
+      animal: 'elefant',
+      breed: 'unknown'
+    },
+    {
+      name:'The Fantastic Mr Fox',
+      animal: 'Fox',
+      breed: 'Red Fox'
+    },
+  ]
+  return React.createElement(
+    "div",
+    {},
+    [
+      React.createElement("h1",{},"Adopt me!"),
+      animals.map(({name, animal, breed}, index)=> React.createElement(Pet, {
+        name,
+        animal,
+        breed,
+        key: index
+      }))
+    ]
+  );
+};
+const container = document.getElementById('root')
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
